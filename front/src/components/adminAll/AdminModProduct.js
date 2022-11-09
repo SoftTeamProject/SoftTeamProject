6bea1a09e16ac3b9c061fe2a8d7f0b1909f43217
anfperezcr <anfperezcr@unal.edu.co>
2022-11-09 00:19:41 -0500

    Avance Front Modificar productos
    Avance Front modificar productos

--- front/src/components/adminAll/AdminModProduct.js
+++ front/src/components/adminAll/AdminModProduct.js
@@ -1,110 +1,190 @@
 import React, { Fragment, useEffect } from 'react'
 
 
-
 export const AdminModProduct = () => {
-return (
-    <Fragment>
+    return (
+        <Fragment>
             
-            <div class="container">
-                <div class="container-fluid text-center">
-                    <div class="row content">
-                        <div class="col-sm-2 sidenav">
-                            <br></br>
-                            <br></br>
-                            <h3>Productos</h3>
-                            <p><a href="http://localhost:3000/">Nintendo DS</a></p>
-                            <p><a href="http://localhost:3000/">Play Station 5</a></p>
-                            <p><a href="http://localhost:3000/">Televisor 55"</a></p>
-                            <p><a href="http://localhost:3000/">Telefono Celular</a></p>
+            <div class="container text-center">
+                <div class="row">
+                
+
+                    {/* Columna izquierda */}
+                    <div class="col-lg-3 well">
+                        <div class="well">
+                            <p><a href="#">Perfil Administrador</a></p>
+                            <img className='card-img-top mx-auto' src='/images/adminIcono.png' alt="Admin"></img>
                         </div>
 
-                        <div class="col-lg-8 text-left">
-                            <h2 >Bienvenido</h2>
-                            <p>Aqui podra modificar los atributos de los diferentes productos, que actualmente se
-                                encuentran en el inventario de su tienda virtual Chronoz Technology.</p>
-                            <hr></hr>
-
-                            <div class="conteiner">
-                                <h3 >Nintendo Switch</h3>
-                                <div class="row">
-                                    <div class="col-lg-4">
-                                        <img src="../images/nintendo.jpg" alt="logo" width="200px"></img>
-                                    </div>
-
-                                    <div class="col-lg-4">
-                                        <div class="container">
-
-                                            <form class="form-horizontal" action="/action_page.php">
-
-                                                <div class="form-group">
-                                                    <label class="control-label col-sm-2" for="email">Nombre:</label>
-                                                    <div class="col-sm-2">
-                                                        <input type="text" class="form-control" id="email" placeholder="Nintendo Switch" name="email"></input>
-                                                    </div>
-                                                </div>
-
-                                                <div class="form-group">
-                                                    <label class="control-label col-sm-2" for="pwd">Descripcion:</label>
-                                                    <div class="col-sm-2">
-                                                        <input type="text" class="form-control" id="pwd" placeholder="" name="pwd"></input>
-                                                    </div>
-                                                </div>
-
-                                                <div class="form-group">
-                                                    <label class="control-label col-sm-2" for="pwd">Precio:</label>
-                                                    <div class="col-sm-2">
-                                                        <input type="text" class="form-control" id="pwd" placeholder="" name="pwd"></input>
-                                                    </div>
-                                                </div>
-
-                                                <div class="form-group">
-                                                    <label class="control-label col-sm-2" for="pwd">Stock:</label>
-                                                    <div class="col-sm-2">
-                                                        <input type="text" class="form-control" id="pwd" placeholder="" name="pwd"></input>
-                                                    </div>
-                                                </div>
-
-                                                <div class="form-group">
-                                                    <div class="col-sm-offset-2 col-sm-2">
-                                                        <button type="submit" class="btn btn-default">Guardar Cambios</button>
-                                                    </div>
-                                                </div>
-                                            </form>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <hr></hr>
-                            <h3>Informacion del Producto</h3>
-                            <p>La consola Nintendo Switch está diseñada para cualquier estilo de vida,
-                                transformándose rápidamente de una consola casera a una consola
-                                portátil.</p>
+                        <div class="well">
+                            <p><a href="#">Andres@gmail.com</a></p>
+                            <p>
+                                <span class="label label-default">Usuario</span>
+                            </p>
                         </div>
 
-                        <div class="col-lg-2 sidenav">
-                            <div class="well">
-                            <div>
-                                <a href='http://localhost:3000/'>
-                                <li>
-                                <i class="fa fa-shopping-cart fa-2x text-white" aria-hidden="true"></i>
-                                <span className="ml-1" id="cart_count" >5</span>
-                                </li>
-                                Cliente</a>
-                                </div>
-                                <p></p>
-                            </div>
-                            <div >
-                                <p></p>
-                            </div>
+                        <div class="alert alert-success fade in">
+                            <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+                            <p><strong>Ey!</strong></p>
+                            People are looking at your profile. Find out who.
                         </div>
-
+                        <p><a href="#">Link</a></p>
+                        
                     </div>
+
+                    {/* Columna central */}
+                    <div class="col-sm-7">
+                        <h1>Campos para Editar el Producto</h1>
+                        <br></br>
+
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <form class="form-inline">Nombre del Producto:
+                                </form>
+                            </div>
+                            <div class="col-sm-9">
+                                <div class="well">
+                                <input type="text" class="form-control" size="50" placeholder="Nombre"></input>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <form>Precio:
+                                </form>
+                            </div>
+                            <div class="col-sm-9">
+                                <div class="well">
+                                <input type="text" class="form-control" size="50" placeholder="Precio"></input>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <form>Descripcion:
+                                </form>
+                            </div>
+                            <div class="col-sm-9">
+                                <div class="well">
+                                <input type="text" class="form-control" size="50" placeholder="Descripcion"></input>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <form>Calificaciones:
+                                </form>
+                            </div>
+                            <div class="col-sm-9">
+                                <div class="well">
+                                <input type="text" class="form-control" size="50" placeholder="Descripcion"></input>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <form>Categoria:
+                                </form>
+                            </div>
+                            <div class="col-sm-9">
+                                <div class="well">
+                                <input type="text" class="form-control" size="50" placeholder="Categoria"></input>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <form>Vendedor:
+                                </form>
+                            </div>
+                            <div class="col-sm-9">
+                                <div class="well">
+                                <input type="text" class="form-control" size="50" placeholder="Vendedor"></input>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <form>Inventario:
+                                </form>
+                            </div>
+                            <div class="col-sm-9">
+                                <div class="well">
+                                <input type="text" class="form-control" size="50" placeholder="Inventario"></input>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <form>Numero Calificaciones:
+                                </form>
+                            </div>
+                            <div class="col-sm-9">
+                                <div class="well">
+                                <input type="text" class="form-control" size="50" placeholder="Calificaciones"></input>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <form>Opiniones:
+                                </form>
+                            </div>
+                            <div class="col-sm-9">
+                                <div class="well">
+                                <input type="text" class="form-control" size="50" placeholder="Opiniones"></input>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <div class="well">
+                                    <p>Jane</p>
+                                    <img className='card-img-top mx-auto' src='./images/play5.jpg' alt="Nutra gold"></img>
+                                </div>
+                            </div>
+                            <div class="col-sm-9">
+                                <div class="well">
+                                    <p>Just Forgot that I had to mention something about someone to someone about how I forgot something, but now I forgot it. Ahh, forget it! Or wait. I remember.... no I don't.</p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    {/* Columna Derecha */}
+                    <div class="col-sm-2 well">
+                        <div class="thumbnail">
+                            <p><h4>Informacion del Producto</h4></p>
+                            <img className='card-img-top mx-auto' src='/images/videojuegos/play5.jpeg' alt="play 5"></img>
+                                <p><strong>Play 5</strong></p>
+                                <p>Viernes 27 Noviembre 2022</p>
+                                <p><strong>$2.999.500</strong></p>
+                                <button class="btn btn-primary">Guardar</button>
+                        </div>
+                        <div class="well">
+                            <p>ADS</p>
+                        </div>
+                    </div>
+                
+
+                
+
                 </div>
+                
             </div>
+            )
+
         </Fragment>
-)
+    )
 }
 
 export default AdminModProduct
